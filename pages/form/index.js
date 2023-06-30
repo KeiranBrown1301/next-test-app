a3410b2bc148b20531682c62c905a0e243622885
KeiranBrown1301 <keiranbrown1301@gmail.com>
2023-06-30 18:20:13 +0900

    first commit, removed some of the base code

---
+++ pages/form/index.js
@@ -0,0 +1,5 @@
+export default function Form() {
+    return (
+        <h1>This is a Form</h1>
+    )
+}
---
+++ pages/tictactoe/index.js
@@ -0,0 +1,5 @@
+export default function TicTacToe() {
+    return (
+        <h1>This is Tic Tac Toe made in react</h1>
+    )
+}
